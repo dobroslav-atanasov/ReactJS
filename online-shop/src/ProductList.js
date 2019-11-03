9bdea090c01658f4a57104cf514debfddda596a2
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-11-03 12:57:53 +0200

    Created component - Product.

--- online-shop/src/ProductList.js
+++ online-shop/src/ProductList.js
@@ -1,22 +1,11 @@
 import React from 'react'
+import Product from './components/product/Product'
 import './ProductList.css'
 
 function ProductList({ product }) {
     return (
         <div className="container">
-            <div className="product-tile">
-                <img className="product-image" src={product.image} alt={product.title} />
-                <div>
-                    <span className="product-brand">{product.brand}</span>
-                    <span className="product-title">{product.title}</span>
-                    <p className="product-price">
-                        Price:
-                <span>
-                            {product.price}$
-                </span>
-                    </p>
-                </div>
-            </div>
+            <Product product={product}/>
         </div>
     )
 }
---
+++ online-shop/src/components/product/Product.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Product = (props) => {
+    return (
+        <div className="product-tile">
+            <img className="product-image" src={props.product.image} alt={props.product.title} />
+            <div>
+                <span className="product-brand">{props.product.brand}</span>
+                <span className="product-title">{props.product.title}</span>
+                <p className="product-price">
+                    Price:
+        <span>
+                        {props.product.price}$
+        </span>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Product
