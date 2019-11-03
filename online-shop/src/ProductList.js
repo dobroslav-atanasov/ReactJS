9bb03fb408395020041867f26a184d185b47b25b
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-11-03 17:36:33 +0200

    Modified ProductList.

--- online-shop/src/ProductList.js
+++ online-shop/src/ProductList.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Product from './components/product/Product'
 import './ProductList.css'
 
-function ProductList({ product }) {
-    return (
-        <div className="container">
-            <Product product={product}/>
-        </div>
-    )
+class ProductList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const { product } = this.props
+        return (
+            <div className="container">
+                <Product product={product} />
+            </div>
+        )
+    }
 }
 
 export default ProductList
