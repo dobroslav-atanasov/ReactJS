be3c5ad6eb3a3b0d9ba70b474218158165740d9f
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-11-07 15:45:41 +0200

    Installed react bootstrap.

--- online-shop/src/index.js
+++ online-shop/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ProductList from './ProductList'
 import data from './data/data'
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<ProductList products={data} />, document.getElementById('root'));
