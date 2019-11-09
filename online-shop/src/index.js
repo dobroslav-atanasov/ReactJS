1e7fc083b131291a121cd4a73d777d0e696a0e92
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-11-09 18:55:47 +0200

    Removed project online-shop.

--- online-shop/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ProductList from './ProductList'
-import data from './data/data'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<ProductList products={data} />, document.getElementById('root'));
