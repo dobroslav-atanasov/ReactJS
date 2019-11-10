6533d52d8d9551484bb78dd2970a3130653f7aa2
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2019-11-10 09:29:44 +0200

    Created post-service to fetch data.

--- Origami-Platform/origami-platform/src/components/Posts/index.js
+++ Origami-Platform/origami-platform/src/components/Posts/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './styles.css';
 import Post from '../Post';
+import postService from '../../services/post-service';
 
-function Posts() {
-    return <div className="Posts">
-        <Post imageUrl="blue-origami-bird.png" imageAlt="blue-origami-bird" author="Dobri">Test post</Post>
-    </div>
+class Posts extends React.Component {
+    state = {
+        posts: null
+    };
+
+    componentDidMount() {
+        postService.load()
+            .then(posts => {
+                this.setState({ posts });
+            });
+    }; 
+
+    render() {
+        const { posts } = this.state;
+        return posts ? <div className="Posts">
+            {posts.map((post) => <Post key={post.id} imageUrl="blue-origami-bird.png" imageAlt="blue-origami-bird" author={post.userId}>{post.body}</Post>)}
+        </div> : <div>Loading...</div>;
+    };
 };
+
+// function Posts() {
+//     return <div className="Posts">
+//         <Post imageUrl="blue-origami-bird.png" imageAlt="blue-origami-bird" author="Dobri">Test post</Post>
+//     </div>
+// };
 
 export default Posts;
